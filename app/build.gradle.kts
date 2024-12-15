a839ab649469fedc0857c9a775d055ee322481cc
jalupriyangga <dionisiusaritojp@gmail.com>
2024-12-15 10:25:41 +0700

    feat:
    Update project configurations and enhance layout with new UI elements.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jalupriyangga.layoutandui"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jalupriyangga.layoutandui"
