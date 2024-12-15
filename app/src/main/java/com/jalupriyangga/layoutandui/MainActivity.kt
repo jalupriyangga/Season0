f8d2f14b354d78707d94bd5de884af9264cc6e33
jalupriyangga <dionisiusaritojp@gmail.com>
2024-12-15 14:00:34 +0700

    feat:
    Create button and change its content by clicking
    Add button click functionality and update layout elements.

--- app/src/main/java/com/jalupriyangga/layoutandui/MainActivity.kt
+++ app/src/main/java/com/jalupriyangga/layoutandui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jalupriyangga.layoutandui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Button button:
+        val button: Button = findViewById(R.id.buttonButton)
+        var isClicked = false
+
+        button.setOnClickListener {
+            if (!isClicked) {
+                Log.i("MainActivity", "Button clicked")
+                button.text = "Clicked"
+            } else {
+                Log.i("MainActivity", "Button unClicked")
+                button.text = "BUTTON"
+            }
+            isClicked = !isClicked
+        }
+
     }
 }
+
