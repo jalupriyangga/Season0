89fca2aaaf76bbe4ce12d04c84da393503a1e76e
jalupriyangga <dionisiusaritojp@gmail.com>
2024-12-15 14:22:45 +0700

    feat: Implement button click to display input text in UI elements

--- app/src/main/java/com/jalupriyangga/layoutandui/MainActivity.kt
+++ app/src/main/java/com/jalupriyangga/layoutandui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.jalupriyangga.layoutandui
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +23,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        //Inputtext
+        val inputEditText: EditText = findViewById(R.id.inputEditText)
+
+
         // Button button:
         val button: Button = findViewById(R.id.buttonButton)
         var isClicked = false
 
         button.setOnClickListener {
+            val input = inputEditText.text.toString()
             if (!isClicked) {
+                Log.i("MainActivity", input)
                 Log.i("MainActivity", "Button clicked")
                 button.text = "Clicked"
+                Toast.makeText(it.context, input, Toast.LENGTH_SHORT).show()
+                Snackbar.make(it, input, Snackbar.LENGTH_SHORT).show()
             } else {
                 Log.i("MainActivity", "Button unClicked")
                 button.text = "BUTTON"
